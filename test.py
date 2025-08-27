5749780c8641d383c9cf8cf8026d9ac6d444d6fb
lamentxu <1372449351@qq.com>
2025-08-28 00:08:35 +0800

    添加test script

---
+++ test.py
@@ -0,0 +1,33 @@
+# test script for Typhon package
+import unittest
+
+from unittest.mock import patch
+from io import StringIO
+from contextlib import redirect_stdout
+
+class TestTyphon(unittest.TestCase):
+    def test_bypassMAIN(self):
+        with redirect_stdout(StringIO()) as f:
+            # case1: some basic WAF
+            from Typhon import bypassMAIN
+            case1 = bypassMAIN(
+            banned_chr=['__builtins__', '__subclasses__'],
+            banned_re='.*import.*',
+            local_scope={'__builtins__': None},
+            max_length=100,
+            log_level='CRITICAL')
+            self.assertTrue(any(i in case1 for i in ['OBJECT', 'TYPE']))
+            # case2: NO waf
+            with patch('builtins.exit') as mock_exit:
+                from Typhon import bypassMAIN
+                bypassMAIN(log_level='CRITICAL')
+                mock_exit.assert_called_with(0)
+            # case3: depth
+            with patch('builtins.exit') as mock_exit:
+                from Typhon import bypassMAIN
+                a = bypassMAIN(depth=0, log_level='CRITICAL', banned_chr=['help'])
+                self.assertFalse('help' in a.values())
+            
+
+if __name__ == '__main__':
+    unittest.main()
