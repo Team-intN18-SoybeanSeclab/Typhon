b35cf9bca5f0f0f802465126b44700561f67014a
lamentxu <1372449351@qq.com>
2025-09-02 18:33:35 +0800

    更好的log_level逻辑

--- test.py
+++ test.py
@@ -15,17 +15,17 @@ class TestTyphon(unittest.TestCase):
             banned_re='.*import.*',
             local_scope={'__builtins__': None},
             max_length=100,
-            log_level='CRITICAL')
+            log_level='TESTING')
             self.assertTrue(any(i in case1 for i in ['OBJECT', 'TYPE']))
             # case2: NO waf
             with patch('builtins.exit') as mock_exit:
                 from Typhon import bypassMAIN
-                bypassMAIN(log_level='CRITICAL')
+                bypassMAIN(log_level='TESTING')
                 mock_exit.assert_called_with(0)
             # case3: depth
             with patch('builtins.exit') as mock_exit:
                 from Typhon import bypassMAIN
-                a = bypassMAIN(depth=0, log_level='CRITICAL', banned_chr=['help'])
+                a = bypassMAIN(depth=0, log_level='TESTING', banned_chr=['help'])
                 self.assertFalse('help' in a.values())
             
 
