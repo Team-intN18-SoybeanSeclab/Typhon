563f8fdc3e3ce2967f407bb6eaad9f777c425029
lamentxu <1372449351@qq.com>
2025-10-04 01:06:33 +0800

    add test case

--- Typhon/Typhon.py
+++ Typhon/Typhon.py
@@ -31,7 +31,7 @@ from .utils import *
 # The RCE data including RCE functions and their parameters.
 from .RCE_data import *
 
-VERSION = "1.0.9.1"
+VERSION = "1.0.10"
 BANNER = (
     r"""
     .-')          _                 Typhon: a pyjail bypassing tool
