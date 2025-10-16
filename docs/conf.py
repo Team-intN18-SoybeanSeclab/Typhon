9af5c96d3f2632892acb586d4c144ea227ca7ff8
Weilin Du <108666168+LamentXU123@users.noreply.github.com>
2025-10-16 22:14:44 +0800

    更换文档主题

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,5 @@ language = "zh-cn"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinxdoc"
+html_theme = "classic"
 html_static_path = ["_static"]
